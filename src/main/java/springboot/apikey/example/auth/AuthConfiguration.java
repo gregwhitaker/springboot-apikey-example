50a195575379c1e08dc319c7a8a9d54356768ca6
gregwhitaker <gwhitake@gmail.com>
2019-01-25 11:55:50 -0800

    Doing work

--- src/main/java/springboot/apikey/example/auth/AuthConfiguration.java
+++ src/main/java/springboot/apikey/example/auth/AuthConfiguration.java
@@ -1,10 +1,22 @@
+/**
+ * Copyright 2019 Greg Whitaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package springboot.apikey.example.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,7 +33,7 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
         ApiKeyAuthFilter filter = new ApiKeyAuthFilter(API_KEY_AUTH_HEADER_NAME);
         filter.setAuthenticationManager(new ApiKeyAuthManager());
 
-        http.antMatcher("/api/**").
+        http.antMatcher("/api/v1/secure").
                 csrf().
                 disable().
                 sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
---
+++ src/main/java/springboot/apikey/example/controller/NonSecureController.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2019 Greg Whitaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package springboot.apikey.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NonSecureController {
+    private static final Logger LOG = LoggerFactory.getLogger(NonSecureController.class);
+
+    @GetMapping(value = "/api/v1/nonsecure")
+    public ResponseEntity nonsecure() {
+        LOG.info("Received request: /api/v1/nonsecure");
+        return ResponseEntity.ok().build();
+    }
+}
