9f4760950fc9bc3a370ea0afb1fd5901ccd2a6ec
gregwhitaker <gwhitake@gmail.com>
2019-01-25 19:03:29 -0800

    Doing work

--- src/main/java/springboot/apikey/example/auth/ApiKeyAuthManager.java
+++ src/main/java/springboot/apikey/example/auth/ApiKeyAuthManager.java
@@ -15,36 +15,77 @@
  */
 package springboot.apikey.example.auth;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import springboot.apikey.example.util.UUIDUtil;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
 
 public class ApiKeyAuthManager implements AuthenticationManager {
     private static final Logger LOG = LoggerFactory.getLogger(ApiKeyAuthManager.class);
 
-    private final DataSource dataSource;
+    private final LoadingCache<String, Boolean> keys;
 
-    @Autowired
-    private ApiKeyAuthManager(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public ApiKeyAuthManager(DataSource dataSource) {
+        this.keys = Caffeine.newBuilder()
+                .expireAfterAccess(5, TimeUnit.MINUTES)
+                .build(new DatabaseCacheLoader(dataSource));
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String principal = (String) authentication.getPrincipal();
 
-        if (!"test".equals(principal))
-        {
+        if (!keys.get(principal)) {
             throw new BadCredentialsException("The API key was not found or not the expected value.");
+        } else {
+            authentication.setAuthenticated(true);
+            return authentication;
+        }
+    }
+
+    /**
+     * Caffeine CacheLoader that checks the database for the api key if it not found in the cache.
+     */
+    private static class DatabaseCacheLoader implements CacheLoader<String, Boolean> {
+        private final DataSource dataSource;
+
+        DatabaseCacheLoader(DataSource dataSource) {
+            this.dataSource = dataSource;
         }
 
-        authentication.setAuthenticated(true);
-        return authentication;
+        @Override
+        public Boolean load(String key) throws Exception {
+            LOG.info("Loading api key from database: [key: {}]", key);
+
+            try (Connection conn = dataSource.getConnection()) {
+                try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM auth WHERE api_key = ?")) {
+                    ps.setObject(1, UUIDUtil.fromHex(key));
+
+                    try (ResultSet rs = ps.executeQuery()) {
+                        if (rs.next()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                LOG.error("An error occurred while retrieving api key from database", e);
+                return false;
+            }
+        }
     }
 }
--- src/main/java/springboot/apikey/example/auth/AuthConfiguration.java
+++ src/main/java/springboot/apikey/example/auth/AuthConfiguration.java
@@ -15,6 +15,7 @@
  */
 package springboot.apikey.example.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,16 +23,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 @Order(1)
 public class AuthConfiguration extends WebSecurityConfigurerAdapter {
     private static final String API_KEY_AUTH_HEADER_NAME = "API_KEY";
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         ApiKeyAuthFilter filter = new ApiKeyAuthFilter(API_KEY_AUTH_HEADER_NAME);
-        filter.setAuthenticationManager(new ApiKeyAuthManager());
+        filter.setAuthenticationManager(new ApiKeyAuthManager(dataSource));
 
         http.antMatcher("/api/v1/secure").
                 csrf().
